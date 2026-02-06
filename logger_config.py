20235f0766c1e14eeffc0bb2d616508ac30fb35b
satar <satar.shams2@gmail.com>
2026-02-06 18:39:58 +0330

    Complete ETL pipeline with Vault integration and logging

--- logger_config.py
+++ logger_config.py
@@ -16,8 +16,8 @@ def get_logger(name: str):
     if not logger.handlers:
         file_handler = logging.FileHandler(LOG_FILE)
         formatter = logging.Formatter(
-            "%(asctime)s | %(levelname)s | %(name)s | PID:%(process)d | %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S.%f",
+            "%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
         )
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
