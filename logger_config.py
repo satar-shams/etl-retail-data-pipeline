7507d1c7b5cd4ec7d3193fa64dea87914eabf5ff
satar <satar.shams2@gmail.com>
2026-02-06 18:21:38 +0330

    Change log timestamp format

--- logger_config.py
+++ logger_config.py
@@ -17,7 +17,7 @@ def get_logger(name: str):
         file_handler = logging.FileHandler(LOG_FILE)
         formatter = logging.Formatter(
             "%(asctime)s | %(levelname)s | %(name)s | %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
+            datefmt="%Y-%m-%d %H:%M:%S.%f",
         )
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
